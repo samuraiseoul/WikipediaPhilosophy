9da15ec65efaa3535d7fff0dc8e74030d4142c57
Scott Lavigne <scott.lavigne@lifestylepubs.com>
2018-02-15 14:18:30 -0600

    cleaned up code to be a bit more readable.

--- src/main/Philosophy.kt
+++ src/main/Philosophy.kt
@@ -1,14 +1,10 @@
-package main
-
-import main.pages.WikipediaPage
 import kotlin.system.exitProcess
 
 const val VICTORY_TITLE = "Philosophy"
-const val WIKIPEDIA_BASE_PATH = "https://en.wikipedia.org"
 const val RANDOM_ARTICLE_URL = "/wiki/Special:Random"
 
 fun main(args: Array<String>){
-    val randomPage = WikipediaPage(WIKIPEDIA_BASE_PATH + RANDOM_ARTICLE_URL)
+    val randomPage = pages.WikipediaPage(pages.WIKIPEDIA_BASE_PATH + RANDOM_ARTICLE_URL)
     println("Starting article: ${randomPage.title}")
     var currentPage = randomPage.nextPage
     val titles = mutableMapOf<String, Int>()
@@ -25,6 +21,7 @@ fun main(args: Array<String>){
             currentPage = currentPage.nextPage
         } catch (exception: NoSuchElementException) {
             println(exception.message)
+            exitProcess(0);
         }
     }
     println(currentPage.title)
--- src/main/pages/WikipediaPage.kt
+++ src/main/pages/WikipediaPage.kt
@@ -1,26 +1,40 @@
-package main.pages
+package pages
 
-import main.WIKIPEDIA_BASE_PATH
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+
+const val WIKIPEDIA_BASE_PATH = "https://en.wikipedia.org"
 
 class WikipediaPage constructor(private val pageUrl: String){
     var index : Int = 0
     private val pageDocument : Document = Jsoup.connect(this.pageUrl).get()
     val title: String = pageDocument.getElementById("firstHeading").text()
     val nextPage by lazy {
-        val useableLinkTags = pageDocument.select("#mw-content-text > div > p > a").filter {
-            it.attr("href").startsWith("/wiki")
-                && !it.attr("href").contains("language", true) //ignore language articles,
-                && !it.attr("title").contains("language", true)
-                && it.parent().html().replace(Regex("\\((.*?)\\)"), "").contains(it.html()) //ignore things in parenthesis
-                && it.siblingElements().none {
-                    it.tagName() == "span" && !it.attr("lang").isNullOrEmpty()
-                }
-        }
-        if((useableLinkTags.lastIndex + 1) == this.index) {
-            throw NoSuchElementException("Infinit Loop, there are no more links to follow that do not lead to infinite loops.")
-        }
-        WikipediaPage(WIKIPEDIA_BASE_PATH + useableLinkTags[this.index].attr("href")) // go to the next link if page has been visited
+        findNextPage()
     }
+
+    private fun findNextPage(): WikipediaPage {
+        val useableLinkTags = pageDocument.select("#mw-content-text > div > p > a").filter {
+            isWikipediaArticle(it) && notLanguage(it) && notWithinParenthesis(it) && noSiblingsWithLanguageElements(it)
+        }
+        if ((useableLinkTags.lastIndex + 1) == this.index) {
+            throw NoSuchElementException("Infinite Loop, there are no more links to follow from this page that do not lead to infinite loops.")
+        }
+        return WikipediaPage(WIKIPEDIA_BASE_PATH + useableLinkTags[this.index].attr("href")) // go to the next link if page has been visited
+    }
+
+    private fun noSiblingsWithLanguageElements(element: Element): Boolean {
+        return element.siblingElements().none {
+            it.tagName() == "span" && !it.attr("lang").isNullOrEmpty()
+        }
+    }
+
+    private fun isWikipediaArticle(element: Element) = element.attr("href").startsWith("/wiki")
+
+    //ignore things in parenthesis
+    private fun notWithinParenthesis(element: Element) = element.parent().html().replace(Regex("\\((.*?)\\)"), "").contains(element.html())
+
+    //ignore language articles
+    private fun notLanguage(element: Element) = !element.attr("href").contains("language", true) && !element.attr("title").contains("language", true)
 }
